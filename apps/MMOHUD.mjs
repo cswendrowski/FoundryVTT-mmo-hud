cb0d98036c620ce4ef95cf91a2666142bb4c84fa
Cody Swendrowski <cody@ironmoosedevelopment.com>
2023-01-09 14:50:05 -0600

    Get real party data, and refresh on update

--- apps/MMOHUD.mjs
+++ apps/MMOHUD.mjs
@@ -37,7 +37,121 @@ export default class MMOHUD extends Application {
     /** @override */
     async getData(options) {
         let data = await super.getData(options);
-        data.party = [{
+        data.party = this._getParty();
+        data.enemy = {
+            id: "y2rIVgjNt6EAU0cY",
+            name: "Big, Bad, Evil Guy",
+            level: 5,
+            primary: {
+                name: "Health",
+                value: 18,
+                max: 20,
+            },
+            effects: [
+                {
+                    name: "Blessed",
+                    icon: "systems/archmage/assets/icons/skills/emerald_01.jpg",
+                    isBuff: true
+                },
+                {
+                    name: "Affliction",
+                    icon: "systems/archmage/assets/icons/skills/affliction_01.jpg",
+                    isDebuff: true
+                },
+                {
+                    name: "Red Crystal",
+                    icon: "systems/archmage/assets/icons/skills/blood_01.jpg",
+                }
+            ]
+        }
+
+        // Calculate percentages
+        data.party.forEach((member) => {
+            member.primary.percent = Math.round((member.primary.value / member.primary.max) * 100);
+            member.secondary.percent = Math.round((member.secondary.value / member.secondary.max) * 100);
+        });
+        if ( data.enemy ) {
+            data.enemy.primary.percent = Math.round((data.enemy.primary.value / data.enemy.primary.max) * 100);
+        }
+
+        // Determine party size
+        data.partySize = this._getPartySize(data.party.length);
+
+        return data;
+    }
+
+    /* -------------------------------------------- */
+
+    /**
+     * Returns the label for the party size
+     * @param {number} size
+     * @returns {string}
+     * @private
+     */
+    _getPartySize(size) {
+        if ( size === 1 ) return "Solo";
+        if ( size === 2 ) return "Duo";
+        if ( size <= 4 ) return "Light Party";
+        if ( size <= 8 ) return "Full Party";
+        return "Alliance";
+    }
+
+    /* -------------------------------------------- */
+
+    _getParty() {
+        // All actors that are assigned to a Player
+        let party = game.users.map(u => u.character).filter(c => c);;
+
+        // If we are in combat, additionally return all actors with disposition of Friendly
+        function _isFriendly(actor) {
+            if ( actor.token ) {
+                return actor.token.disposition === CONST.TOKEN_DISPOSITIONS.FRIENDLY;
+            }
+            else return actor.prototypeToken.disposition === CONST.TOKEN_DISPOSITIONS.FRIENDLY;
+        }
+        if ( game.combat ) {
+            const friendlyCombatants = game.combat.combatants
+                .filter(c => _isFriendly(c.actor))
+                .map(c => c.actor);
+            party = party.concat(friendlyCombatants);
+        }
+        console.dir(party);
+
+        // Translate the actor data into the format we need
+        return party.map(actor => {
+            return {
+                id: actor.id,
+                name: actor.name,
+                level: actor.system.details.level.value,
+                image: actor.img,
+                primary: {
+                    name: actor.system.attributes.hp.label,
+                    value: actor.system.attributes.hp.value,
+                    max: actor.system.attributes.hp.max,
+                    theme: "rpg-t-hp"
+                },
+                secondary: {
+                    name: actor.system.attributes.recoveries.label,
+                    value: actor.system.attributes.recoveries.value,
+                    max: actor.system.attributes.recoveries.max,
+                    theme: "rpg-t-mp"
+                },
+                effects: actor.effects.map(e => {
+                    return {
+                        name: e.label,
+                        icon: e.icon,
+                        isBuff: e.changes.some(c => c.value > 0),
+                        isDebuff: e.changes.some(c => c.value < 0),
+                    }
+                })
+            }
+        });
+    }
+
+    /* -------------------------------------------- */
+
+    _getMockPartyData() {
+        return [{
             id: "y2rIVgjNt6EAU0cY",
             name: 'Mr. Dragonborn',
             level: 5,
@@ -105,62 +219,6 @@ export default class MMOHUD extends Application {
                 }
             ]
         }];
-        data.enemy = {
-            id: "y2rIVgjNt6EAU0cY",
-            name: "Big, Bad, Evil Guy",
-            level: 5,
-            primary: {
-                name: "Health",
-                value: 18,
-                max: 20,
-            },
-            effects: [
-                {
-                    name: "Blessed",
-                    icon: "systems/archmage/assets/icons/skills/emerald_01.jpg",
-                    isBuff: true
-                },
-                {
-                    name: "Affliction",
-                    icon: "systems/archmage/assets/icons/skills/affliction_01.jpg",
-                    isDebuff: true
-                },
-                {
-                    name: "Red Crystal",
-                    icon: "systems/archmage/assets/icons/skills/blood_01.jpg",
-                }
-            ]
-        }
-
-        // Calculate percentages
-        data.party.forEach((member) => {
-            member.primary.percent = Math.round((member.primary.value / member.primary.max) * 100);
-            member.secondary.percent = Math.round((member.secondary.value / member.secondary.max) * 100);
-        });
-        if ( data.enemy ) {
-            data.enemy.primary.percent = Math.round((data.enemy.primary.value / data.enemy.primary.max) * 100);
-        }
-
-        // Determine party size
-        data.partySize = this._getPartySize(data.party.length);
-
-        return data;
-    }
-
-    /* -------------------------------------------- */
-
-    /**
-     * Returns the label for the party size
-     * @param {number} size
-     * @returns {string}
-     * @private
-     */
-    _getPartySize(size) {
-        if ( size === 1 ) return "Solo";
-        if ( size === 2 ) return "Duo";
-        if ( size <= 4 ) return "Light Party";
-        if ( size <= 8 ) return "Full Party";
-        return "Alliance";
     }
 
     /* -------------------------------------------- */
--- scripts/mmo-hud.js
+++ scripts/mmo-hud.js
@@ -9,3 +9,10 @@ Hooks.once('ready', async function() {
 Hooks.once('canvasReady', async function() {
     MMOHUD.init();
 });
+
+Hooks.on("updateActor", (actor, data, options, userId) => {
+    // Update MMO HUD
+    if (ui.mmoHud) {
+        ui.mmoHud.render();
+    }
+});
