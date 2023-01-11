9732e948fe9f0519aeedc8bb4ace618e7aa30e80
Cody Swendrowski <cody@ironmoosedevelopment.com>
2023-01-10 18:46:41 -0600

    Support multiple enemies. Add temp health indication

--- apps/MMOHUD.mjs
+++ apps/MMOHUD.mjs
@@ -38,41 +38,64 @@ export default class MMOHUD extends Application {
     async getData(options) {
         let data = await super.getData(options);
         data.party = this._getParty();
-        data.enemy = {
-            id: "y2rIVgjNt6EAU0cY",
-            name: "Big, Bad, Evil Guy",
-            level: 5,
-            primary: {
-                name: "Health",
-                value: 18,
-                max: 20,
+        data.enemies = [
+            {
+                id: "y2rIVgjNt6EAU0cY",
+                name: "Big, Bad, Evil Guy",
+                level: 5,
+                primary: {
+                    name: "Health",
+                    value: 18,
+                    max: 20,
+                },
+                effects: [
+                    {
+                        name: "Blessed",
+                        icon: "systems/archmage/assets/icons/skills/emerald_01.jpg",
+                        isBuff: true
+                    },
+                    {
+                        name: "Affliction",
+                        icon: "systems/archmage/assets/icons/skills/affliction_01.jpg",
+                        isDebuff: true
+                    },
+                    {
+                        name: "Red Crystal",
+                        icon: "systems/archmage/assets/icons/skills/blood_01.jpg",
+                    }
+                ]
             },
-            effects: [
-                {
-                    name: "Blessed",
-                    icon: "systems/archmage/assets/icons/skills/emerald_01.jpg",
-                    isBuff: true
+            {
+                id: "y2rIVgjNt6EAU0cY",
+                name: "Smaller, Bad, Evil Guy",
+                level: 5,
+                primary: {
+                    name: "Health",
+                    value: 5,
+                    max: 10,
                 },
-                {
-                    name: "Affliction",
-                    icon: "systems/archmage/assets/icons/skills/affliction_01.jpg",
-                    isDebuff: true
-                },
-                {
-                    name: "Red Crystal",
-                    icon: "systems/archmage/assets/icons/skills/blood_01.jpg",
-                }
-            ]
-        }
+                effects: [
+                    {
+                        name: "Blessed",
+                        icon: "systems/archmage/assets/icons/skills/emerald_01.jpg",
+                        isBuff: true
+                    }
+                ]
+            }
+        ]
 
         // Calculate percentages
         data.party.forEach((member) => {
-            member.primary.percent = Math.round((member.primary.value / member.primary.max) * 100);
+            const totalPrimary = member.primary.max + (member.primary.temp ?? 0);
+            member.primary.percent = Math.round((member.primary.value / totalPrimary) * 100);
+            if ( member.primary.temp ) {
+                member.primary.bonusPercent = Math.round((member.primary.temp / totalPrimary) * 100);
+            }
             member.secondary.percent = Math.round((member.secondary.value / member.secondary.max) * 100);
         });
-        if ( data.enemy ) {
-            data.enemy.primary.percent = Math.round((data.enemy.primary.value / data.enemy.primary.max) * 100);
-        }
+        data.enemies.forEach((enemy) => {
+            enemy.primary.percent = Math.round((enemy.primary.value / enemy.primary.max) * 100);
+        });
 
         // Determine party size
         data.partySize = this._getPartySize(data.party.length);
@@ -128,6 +151,7 @@ export default class MMOHUD extends Application {
                     name: actor.system.attributes.hp.label,
                     value: actor.system.attributes.hp.value,
                     max: actor.system.attributes.hp.max,
+                    temp: actor.system.attributes.hp.temp,
                     theme: "rpg-t-hp"
                 },
                 secondary: {
